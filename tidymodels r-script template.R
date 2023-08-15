edd143a547ea9611021365b0dfe1a54089c86760
pw2 <pward2@gmail.com>
2023-08-14 19:14:57 -0700

    Add files via upload

---
+++ tidymodels r-script template.R
@@ -0,0 +1,30 @@
+
+############# Tidymodels r-script template ###############
+
+#### Load packages and data -----------------------------
+library(tidyverse)
+library(tidymodels)
+
+
+#### EDA -----------------------------------------------
+
+
+#### Train/Test Split ----------------------------------
+
+#### Cross Validation Split  ---------------------------
+
+#### Model Recipe  ------------------------------------
+
+#### Model Engine  ------------------------------------
+
+#### Workflow  ----------------------------------------
+
+#### Hyperparameter tuning  ---------------------------
+
+#### View the model performance -----------------------
+
+#### Extract the best model  --------------------------
+
+#### Fit the final model  -----------------------------
+
+#### Save Model & Make Predictions on New Data --------
